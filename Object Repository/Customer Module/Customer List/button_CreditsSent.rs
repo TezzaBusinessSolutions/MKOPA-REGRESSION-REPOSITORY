e1629be120f71209eeee08fc590cc500058ac946
stellah.ireri <stellah.ireri@KE-HQ-000926.M-KOPA.local>
2018-01-25 16:07:51 +0300

    credit purchase

---
+++ Object Repository/Customer Module/Customer List/button_CreditsSent.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>information about the credits sent to a device upon making a payment</description>
+   <name>button_CreditsSent</name>
+   <tag></tag>
+   <elementGuidId>a1e4e9ea-7739-42c1-81bb-e7c1e46f6b3c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//a[contains(.,'Credits Sent')]</value>
+   </webElementProperties>
+</WebElementEntity>
