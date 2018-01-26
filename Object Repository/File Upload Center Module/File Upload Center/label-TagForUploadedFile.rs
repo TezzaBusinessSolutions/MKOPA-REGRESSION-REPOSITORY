c9974e5ad6323190cd8782e57051c6d5d919568f
Gituto <dennisgituto1@gmail.com>
2018-01-26 10:41:53 +0300

    Complete Test cases

---
+++ Object Repository/File Upload Center Module/File Upload Center/label-TagForUploadedFile.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Tag for Uploaded File</description>
+   <name>label-TagForUploadedFile</name>
+   <tag></tag>
+   <elementGuidId>8b1973cf-3617-43e3-8d2a-bb0e7d40f839</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[.='${tagForUpload}']</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/Payments Module/Edit Cash Payment Plan/input_Plan Display Name.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Plan Display Name</description>
+   <name>input_Plan Display Name</name>
+   <tag></tag>
+   <elementGuidId>4c95738a-85e5-4c29-875a-48af850955cd</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>.//*[@id='displayName']</value>
+   </webElementProperties>
+</WebElementEntity>
