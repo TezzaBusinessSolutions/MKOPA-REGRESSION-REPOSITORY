6588094bc8400031ded82e7c593fd21e7954fae1
Moukatech <lewismocha@gmail.com>
2018-01-26 13:05:23 +0300

    Ticks updates

---
+++ Object Repository/File Upload Center Module/File Upload Center/Closefileuploaded view.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Closefileuploaded view</name>
+   <tag></tag>
+   <elementGuidId>1a94d707-2eb8-422a-b514-daefa0ea5ce4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>.//*[@id='file-upload-center-details-window']//span[text()='Close']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>.//*[@id='file-upload-center-details-window']//span[text()='Close']</value>
+   </webElementProperties>
+</WebElementEntity>
