adc1f11672a1eb8329bacf4c415234bf8701978c
stellah.ireri <stellah.ireri@KE-HQ-000926.M-KOPA.local>
2018-01-31 15:13:57 +0300

    Updates

--- Object Repository/Customer Module/Customer List/credits.rs
+++ Object Repository/Customer Module/Customer List/credits.rs
@@ -1,13 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebElementEntity>
-   <description>Number of cedit sent on a device</description>
-   <name>label_SentCredit</name>
+   <description></description>
+   <name>credits</name>
    <tag></tag>
-   <elementGuidId>02ca5a08-8546-42b9-9985-894b9c688848</elementGuidId>
+   <elementGuidId>4d75619c-193a-4c59-9841-be5c7ef40a27</elementGuidId>
    <selectorCollection>
       <entry>
          <key>BASIC</key>
-         <value>.//*[@id='OutboundCredits']/table/tbody/tr[td[text()=&quot;${currentCredits}&quot;]]</value>
+         <value></value>
       </entry>
    </selectorCollection>
    <selectorMethod>BASIC</selectorMethod>
@@ -17,6 +17,6 @@
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value>.//*[@id='OutboundCredits']/table/tbody/tr[td[text()=&quot;${currentCredits}&quot;]]</value>
+      <value>.//*[@id='div-account-info']/div[1]/div/div[5]/div[1]/div[2]</value>
    </webElementProperties>
 </WebElementEntity>
